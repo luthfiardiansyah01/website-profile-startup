540ee2cce7d8eae1d2703ef1e528ab1c3217213e
luthfiardiansyah01 <luthfiardiansyah2001@gmail.com>
2026-05-01 14:41:04 +0700

    Updated index.ts

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -8,7 +8,8 @@ export const translations = {
     donate: 'Donate',
 
     // Hero Section
-    heroTitle: 'Pioneering The Future of Innovations',
+    heroTitle: 'Pioneering The',
+    hero2Title: 'Future of Innovations'
     heroSubtitle: 'Accelerating innovation through AI-powered infrastructure for startups and enterprises',
     heroGetStarted: 'Get Started',
     heroLearnMore: 'Learn More',
