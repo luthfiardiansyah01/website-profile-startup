50f5b875e22b93c74657af533c59b1188d0dc813
luthfiardiansyah01 <luthfiardiansyah2001@gmail.com>
2026-05-04 14:38:29 +0700

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeroSection from '../components/home/HeroSection';
 import ProgramSection from '../components/home/ProgramSection';
 import PricingSection from '../components/home/PricingSection';
-import SponsorSection from '../components/home/SponsorSection';
+import SponsorSection from '../components/home/PartnershipSection';
 
 const HomePage = () => {
   return (
